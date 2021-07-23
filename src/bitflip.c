42e27fc66ff994cb0d22811a7d93c50702f80ba8
Devon Fears <devonfearssub@gmail.com>
2021-07-23 15:07:56 -0500

    Done some tests trying to change utf8char type to char * type

--- src/bitflip.c
+++ src/bitflip.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-#include "unichar.c"
+#include "unichar.h"
+#include <string.h>
 
 int main()
 {
     utf8char g = {0xf0,0x9f,0x98,0x80}; //GRINNING FACE emoji
     utf8char a = {0xe2,0xac,0x86}; //UPWARDS BLACK ARROW
 
-    unsigned char * c = tochars(a);
-    printf("%d\n",sizeof(a));
+    unsigned char * b = utf8toString(g);
+    unsigned char * c = appendToString(b,a);
 
-    c = c + 3; //Pointing to an empty space?
-    printf("%.2x\n",*c);
+    printf("%.8x\n",b);
+    printf("%.8x\n",c);
+    printf("\n");
+    printf("%s",b);
+    printf("%s",c);
 }
--- src/unichar.c
+++ src/unichar.c
@@ -1,10 +1,9 @@
 #include "unichar.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct utf8char {
-    unsigned char hex[4];
-};
-
-void printchar(utf8char a)
+void printutf8char(utf8char a)
 {
     for(short i = 0; i < 4; i++)
     {
@@ -12,8 +11,47 @@ void printchar(utf8char a)
     }
 }
 
-char * tochars(utf8char a)
+unsigned char * utf8toString(utf8char a)
 {
     unsigned char * ret = a.hex;
     return ret;
+}
+
+//This guys use is somewhat unknown, 
+//All I really want is to 
+unsigned char * append(utf8char a, utf8char b)
+{
+    int new_size = strlen(a.hex)+strlen(b.hex);
+    char * ret = (char *) calloc(new_size,sizeof(char));
+
+    for(int i = 0; i < strlen(a.hex); i++)
+    {
+        * ret = a.hex[i];
+        ret++;
+    }
+
+    for(int i = 0; i < strlen(b.hex); i++)
+    {
+        * ret = b.hex[i];
+        ret++;
+    }
+    return (unsigned char *) ret;
+}
+
+unsigned char * appendToString(unsigned char * string, utf8char b)
+{
+    int new_size = strlen(string)+strlen(b.hex);
+    unsigned char * ret = (unsigned char *) calloc(new_size,sizeof(unsigned char));
+
+    for(int i = 0; i < strlen(string); i ++)
+    {
+
+    }
+
+    for(int i = 0; i < strlen(b.hex); i++)
+    {
+        * ret = b.hex[i];
+        ret++;
+    }
+    return (unsigned char *) ret;
 }
